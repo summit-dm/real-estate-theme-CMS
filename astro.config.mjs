20a622b3acdc4b689d46744cd20e1543e68e0404
Kyle Sturman <kyle@summitdesignmanagement.com>
2024-04-18 14:56:26 -0500

    adding tailwind config and vite https to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    }
+  }
+});
